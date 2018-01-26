5c6569e1e29ed0abb192c5e331383635af2b5be6
mklimowicz <maciej.klimowicz@rb.com>
2018-01-26 12:42:40 +0100

    package update

---
+++ types/jquery-visible/index.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="node" />
+
+
+declare module "jquery-visible" {
+  var visible: any;
+  export default visible;
+}
+
+
+/*
+export class Visible {
+  constructor(first: any, last: any);
+  first: any;
+  last: any;
+}
+export default Visible;
+*/
